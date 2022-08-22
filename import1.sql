b740809b6199b60320b731960ee0e693b6716fc9
BZAghalarov <aghalarovbehruz@gmail.com>
2022-08-22 14:21:30 +0300

    Added migration files

--- import1.sql
+++ import1.sql
@@ -1,6 +1,13 @@
-LOAD DATA INFILE '/home/bahruz/projects/tutorial/bahruz-aghalar/Hudson_Link/agency.txt'
-INTO TABLE schedule_agency
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- LOAD DATA INFILE '/home/bahruz/projects/tutorial/bahruz-aghalar/Hudson_Link/agency.txt'
+-- INTO TABLE schedule_agency
+-- FOR FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+
+COPY schedule_agency
+FROM '/home/bahruz/projects/tutorial/bahruz-aghalar/cedarsystems/Hudson_Link/agency.txt'
+DELIMITER ','
+CSV HEADER;
+
--- schedule/models.py
+++ schedule/models.py
@@ -11,3 +11,73 @@ class Agency(models.Model):
     agency_phone = models.CharField(max_length=30)
     agency_fare_url = models.URLField()
 
+
+class Calendar(models.Model):
+    service_id = models.CharField(max_length=25)
+    monday = models.CharField(max_length=25)
+    tuesday = models.CharField(max_length=25)
+    wednesday = models.CharField(max_length=25)
+    thursday = models.CharField(max_length=25)
+    friday = models.CharField(max_length=25)
+    saturday = models.CharField(max_length=25)
+    sunday = models.CharField(max_length=25)
+    start_date = models.CharField(max_length=25)
+    end_date = models.CharField(max_length=25)
+
+
+class CalendarDates(models.Model):
+    service_id = models.CharField(max_length=25)
+    date = models.CharField(max_length=25)
+    exception_type = models.CharField(max_length=25)
+
+
+class Routes(models.Model):
+    route_id = models.CharField(max_length=25)
+    route_short_name = models.CharField(max_length=45)
+    route_long_name = models.CharField(max_length=125)
+    route_desc = models.CharField(max_length=125, null=True)
+    route_type = models.CharField(max_length=25)
+    route_url = models.CharField(max_length=125, null=True)
+    route_color = models.CharField(max_length=50, null=True)
+    route_text_color = models.CharField(max_length=25, null=True)
+
+
+class Shapes(models.Model):
+    shape_id = models.CharField(max_length=35)
+    shape_pt_lat = models.CharField(max_length=45)
+    shape_pt_lon = models.CharField(max_length=45)
+    shape_pt_sequence = models.IntegerField()
+
+
+class Stops(models.Model):
+    stop_id = models.CharField(max_length=20)
+    stop_code = models.IntegerField()
+    stop_name = models.CharField(max_length=125)
+    stop_desc = models.CharField(max_length=100, null=True)
+    stop_lat = models.CharField(max_length=100)
+    stop_lon = models.CharField(max_length=100)
+    zone_id = models.IntegerField(null=True)
+    stop_url = models.URLField(null=True, blank=True)
+    location_type = models.CharField(max_length=100, null=True)
+    parent_station= models.CharField(max_length=100, null=True)
+    wheelchair_boarding = models.CharField(max_length=100, null=True)
+
+
+class StopTimes(models.Model):
+    trip_id = models.CharField(max_length=75)
+    arrival_time = models.CharField(max_length=100)
+    departure_time = models.CharField(max_length=100)
+    stop_id = models.CharField(max_length=100)
+    stop_sequence = models.IntegerField()
+    pickup_type = models.CharField(max_length=100)
+    drop_off_type = models.CharField(max_length=100)
+
+
+class Trips(models.Model):
+    route_id = models.CharField(max_length=100)
+    service_id = models.CharField(max_length=100)
+    trip_id = models.CharField(max_length=100)
+    trip_headsign = models.CharField(max_length=100)
+    direction_id = models.CharField(max_length=100)
+    block_id = models.CharField(max_length=100)
+    shape_id = models.CharField(max_length=100)
